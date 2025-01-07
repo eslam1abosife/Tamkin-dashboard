d5569c47d4b94bd04f636e450ddadc6ed3836105
basem <basemgaber01111@gmail.com>
2025-01-07 17:29:55 +0200

    Show Icon Defult Site

--- tamkin-dashboard/pages/my-site.vue
+++ tamkin-dashboard/pages/my-site.vue
@@ -660,18 +660,19 @@ const openInvestor = (app, pack) => {
                 "
                 class="w-[40px] h-[40px] bg-[#2DADA3] rounded-full text-white flex items-center justify-center"
               >
-              <span v-if="!apps.length">
+              <!-- <span v-if="!apps.length"> -->
+              <span>
                 <img
                 src="/assets/imgs/icons/mysite_select.svg"
                 class="w-[40px] h-[40px]"
               
               />
               </span>
-               <span v-else>
+               <!-- <span v-else>
                 {{
                   defaultApp&&  defaultApp?.title ? getAvatarLetters(defaultApp?.title) : ""
                 }}
-               </span>
+               </span> -->
                 
               </div>
               <div
