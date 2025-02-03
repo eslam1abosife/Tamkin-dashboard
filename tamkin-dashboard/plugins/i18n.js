40411c74de8d624b04ca54cf5c711f1474166524
Basem <basemgaber01111@gmail.com>
2025-02-03 23:36:19 +0200

    Tasks

--- tamkin-dashboard/plugins/i18n.js
+++ tamkin-dashboard/plugins/i18n.js
@@ -2,23 +2,39 @@ export default defineNuxtPlugin(async (nuxtApp) => {
   const config = useRuntimeConfig();
 
   // Fetch languages dynamically from your API
-  const data = await $fetch(`${config.public.baseURL}Language/Get`, {
-    method: 'POST',
-    body: {
-      where: {
-        enabled: 1
-      }
-    }
-  });
+  // const data = await $fetch(`${config.public.baseURL}Language/Get`, {
+  //   method: 'POST',
+  //   body: {
+  //     where: {
+  //       enabled: 1
+  //     }
+  //   }
+  // });
 
   // Map the response to the format expected by nuxt-i18n
-  const languages = ref(data.data.map(lang => ({
-    code: lang.name,
-    name: lang.language_name,
-    file: `${lang.name}.ts`,
-    iso: lang.name === 'en' ? 'en-US' : 'ar-SA',
-    dir: lang.name === 'ar' ? "rtl" : "ltr"
-  })));
+  // const languages = ref(data.data.map(lang => ({
+  //   code: lang.name,
+  //   name: lang.language_name,
+  //   file: `${lang.name}.ts`,
+  //   iso: lang.name === 'en' ? 'en-US' : 'ar-SA',
+  //   dir: lang.name === 'ar' ? "rtl" : "ltr"
+  // })));
+  const languages = ref([
+    {
+      code: 'en',
+      name: 'English - EN',
+      file: 'en.ts',
+      iso: 'en-US',
+      dir: 'ltr'
+    },
+    {
+      code: 'ar',
+      name: 'العربية - AR',
+      file: 'ar.ts',
+      iso: 'ar-SA',
+      dir: 'rtl'
+    }
+  ]);
 
   // Set the dynamic locales
   nuxtApp.$i18n.locales = languages;
